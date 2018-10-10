ced11391ac1cf3697a151e541c680bf91708c2e6
Kyle van den Boom <520163@student.glu.nl>
2018-10-10 11:08:58 +0200

    Added Game
    Whole game

---
+++ glupartygame/Assets/Minigames/PlaneAway/Scripts/PlaneAwayGameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XBOXParty;
+
+namespace TestProject
+{
+    public class PlaneAwayGameManager : MonoBehaviour
+    {
+        private List<int> m_positions;
+        [SerializeField] List<GameObject> m_allplayers;
+        List<GameObject> m_currentaliveplayers;
+        int m_playercount;
+
+        void Awake()
+        {
+            //The pure purpose of this list is to let you know who ended on place(position) 1, place 2 and so on...
+            m_positions = new List<int>();
+
+            m_currentaliveplayers = new List<GameObject>();
+
+            m_playercount = GlobalGameManager.Instance.PlayerCount;
+
+            for (int i = 0; i < m_playercount; ++i)
+            {
+                Debug.Log("Playercount : "+m_playercount);
+
+                //Only sets the playing players active and gives them the number they are playing as
+                m_allplayers[i].SetActive(true);
+                m_allplayers[i].GetComponent<PlayerController>().playerNumber = i;
+                //m_allplayers[i].GetComponent<PlayerController>().SetPlayerColor(GlobalGameManager.Instance.GetPlayerColor(i));
+                
+
+                //Binding every button per player ---- ONLY WORKS IF THERE IS NO KEYBOARD PLAYER
+                //InputManager.Instance.BindAxis("PlaneAway_Horizontal_P" + m_playercount, m_playercount, ControllerAxisCode.LeftStickX);
+                //InputManager.Instance.BindButton("PlaneAway_Jump_P" + m_playercount, m_playercount, ControllerButtonCode.A, ButtonState.OnPress);
+            }
+
+            for (int p = 0; p < m_allplayers.Count ; p++)
+            {
+                if(m_allplayers[p].gameObject.activeSelf == true)
+                {
+                    m_currentaliveplayers.Add(m_allplayers[p]);
+                }
+            }
+
+            InputManager.Instance.BindAxis("PlaneAway_Horizontal_P0", KeyCode.D, KeyCode.A);
+            InputManager.Instance.BindAxis("PlaneAway_Horizontal_P1", 0, ControllerAxisCode.LeftStickX);
+            InputManager.Instance.BindAxis("PlaneAway_Horizontal_P2", 1, ControllerAxisCode.LeftStickX);
+            InputManager.Instance.BindAxis("PlaneAway_Horizontal_P3", 2, ControllerAxisCode.LeftStickX);
+
+            InputManager.Instance.BindButton("PlaneAway_Jump_P0", KeyCode.W, ButtonState.OnPress);
+            InputManager.Instance.BindButton("PlaneAway_Jump_P1", 0, ControllerButtonCode.A, ButtonState.OnPress);
+            InputManager.Instance.BindButton("PlaneAway_Jump_P2", 1, ControllerButtonCode.A, ButtonState.OnPress);
+            InputManager.Instance.BindButton("PlaneAway_Jump_P3", 2, ControllerButtonCode.A, ButtonState.OnPress);
+        }
+
+        void Update()
+        {
+            Debug.Log("How many players alive? " + m_currentaliveplayers.Count);
+
+            if (m_currentaliveplayers.Count < 2)
+            {
+                UpdateStandings(m_currentaliveplayers[0].GetComponent<PlayerController>().playerNumber);
+                SubmitEndResult();
+            }
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            UpdateStandings(other.gameObject.GetComponent<PlayerController>().playerNumber);
+            m_currentaliveplayers.Remove(other.gameObject);
+        }
+
+        public void UpdateStandings(int playerid)
+        {
+            m_positions.Add(playerid);
+        }
+
+        public void SubmitEndResult()
+        {
+            GlobalGameManager.Instance.SubmitGameResults(m_positions);
+        }
+    }
+}
---
+++ glupartygame/Assets/Minigames/PlaneAway/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XBOXParty;
+
+namespace TestProject
+{
+    public class PlayerController : MonoBehaviour
+    {
+        int m_playernumber;
+        float m_movementspeed;
+
+        bool m_onground;
+
+        Rigidbody m_myrigibody;
+        Vector3 m_movevelocity;
+
+        //Material m_material;
+
+        void Start()
+        {
+            m_movementspeed = 7;
+            m_myrigibody = GetComponent<Rigidbody>();
+            //m_material = GetComponent<Material>();
+        }
+
+        void Update()
+        {
+            Move(m_playernumber);
+            Jump(m_playernumber);
+        }
+
+        void Move(int playernumber)
+        {
+            Vector2 moveinput = new Vector2(InputManager.Instance.GetAxis("PlaneAway_Horizontal_P" + m_playernumber), 0f);
+            m_movevelocity = (moveinput.normalized * m_movementspeed);
+            m_myrigibody.MovePosition(m_myrigibody.position + m_movevelocity * Time.deltaTime);
+        }
+
+        void Jump(int playernumber)
+        {
+            if (InputManager.Instance.GetButton("PlaneAway_Jump_P" + playernumber) && m_onground == true)
+            {
+                m_myrigibody.velocity = new Vector3(0, 8.5f, 0);
+                m_onground = false;
+            }
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.collider.CompareTag("Tag 0"))
+            {
+                m_onground = true;
+            }
+        }
+
+        /*
+        public void SetPlayerColor(Color color)
+        {
+            m_material.color = color;   
+        }
+        */
+
+        public int playerNumber
+        {
+            get { return m_playernumber; }
+            set { m_playernumber = value; }
+        }
+    }
+}
